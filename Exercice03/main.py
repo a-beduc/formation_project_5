e29096291651b3ac124015367025d1c5cda449bf
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-10-03 17:40:23 +0200

    Exercice 3

--- Exercice03/main.py
+++ Exercice03/main.py
@@ -1,1 +1,9 @@
-words = ["python", "programmation", "langage", "ordinateur", "apprentissage"]
+def main():
+    words = ["python", "programmation", "langage", "ordinateur", "apprentissage"]
+    vowels = {'a', 'e', 'i', 'o', 'u', 'y', 'é', 'à', 'è', 'ù', 'â', 'ê', 'î', 'ô', 'û'}
+    list_of_word_vowel = [(word, len([i for i in word.lower() if i in vowels])) for word in words]
+    print(list_of_word_vowel)
+
+
+if __name__ == '__main__':
+    main()
