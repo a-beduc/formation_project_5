92494ec0288e8f47b6764f2f84658b92c284543c
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-10-03 17:48:59 +0200

    Exercice 5

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,22 @@
 def sum(a, b):
+    """
+    Return the sum of two numbers.
+            Parameters:
+                    a : A number
+                    b : A number
+            Returns:
+                    a + b
+    """
     return a + b
- 
+
+
 def subtraction(a, b):
+    """
+    Return the subtraction of two numbers.
+        Parameters:
+                a : A number
+                b : A number
+        Returns:
+                a + b
+    """
     return a - b
