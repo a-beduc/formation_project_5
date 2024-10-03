9eeea3c77e7edf3e113a1731f9f0f2101fb79f62
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-10-03 17:58:34 +0200

    Exercice 6

--- Exercice06/main.py
+++ Exercice06/main.py
@@ -1,6 +1,16 @@
-# Fonction calculate_average
- 
-# Exemple d'utilisation de la fonction
-numbers = [10, 20, 30, 40, 50]
-average = calculate_average(numbers)
-print("La moyenne est :", average)
+def calculate_average(numbers):
+    try:
+        return sum(numbers) / len(numbers)
+    except ZeroDivisionError:
+        return 0
+
+
+def main():
+    # Exemple d'utilisation de la fonction
+    numbers = [10, 20, 30, 40, 50]
+    average = calculate_average(numbers)
+    print("La moyenne est :", average)
+
+
+if __name__ == "__main__":
+    main()
