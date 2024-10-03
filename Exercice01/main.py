3628acde25a7a54ada2fdb469d9a570323055b0e
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-10-03 17:03:37 +0200

    Exercice 1

--- Exercice01/main.py
+++ Exercice01/main.py
@@ -1,2 +1,8 @@
-name = input("Entrez votre nom : ")
-age = int(input("Entrez votre âge : "))
+def main():
+    name = input("Entrez votre nom : ")
+    age = int(input("Entrez votre âge : "))
+    print(f"Bonjour, je m'appelle {name} et j'ai {age} ans.")
+
+
+if __name__ == "__main__":
+    main()
