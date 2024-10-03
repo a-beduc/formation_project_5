8ea9692d3daa8b8475764d34fd5dca0f9a56ddfc
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-10-03 18:06:27 +0200

    Exercice 7

--- Exercice07/main.py
+++ Exercice07/main.py
@@ -1,1 +1,21 @@
-## Écrivez votre code ici !
+def square(a):
+    try:
+        return a * a
+    except TypeError:
+        print("Le paramètre doit être un nombre !")
+        return None
+
+
+def main():
+    a = -4.046046
+    b = 'a'
+    c = [4, 6]
+    d = {"a": 12}
+    print(square(a))
+    print(square(b))
+    print(square(c))
+    print(square(d))
+
+
+if __name__ == '__main__':
+    main()
