d1bfdc3e87a8895cd0022bb1338520fed78254e8
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-10-03 17:12:33 +0200

    Exercice 2

--- Exercice02/main.py
+++ Exercice02/main.py
@@ -1,17 +1,34 @@
-students = {
-    'Alice': {
-         'Mathematiques': 90,
-         'Francais': 80,
-         'Histoire': 95
-    },
-    'Bob': {
-         'Mathematiques': 75,
-         'Francais': 85,
-         'Histoire': 70
-    },
-     'Charlie': {
-         'Mathematiques': 88,
-         'Francais': 92,
-         'Histoire': 78
-     }
-}
+def main():
+    students = {
+        'Alice': {
+            'Mathematiques': 90,
+            'Francais': 80,
+            'Histoire': 95
+        },
+        'Bob': {
+            'Mathematiques': 75,
+            'Francais': 85,
+            'Histoire': 70
+        },
+        'Charlie': {
+            'Mathematiques': 88,
+            'Francais': 92,
+            'Histoire': 78
+        }
+    }
+
+    student_name = input("Entrez le nom de l’étudiant : ").capitalize()
+    if student_name in students.keys():
+        print(f"Notes de{student_name}")
+        grades_sum = 0
+        for key, value in students[student_name].items():
+            print(f"{key}: {value}")
+            grades_sum += value
+        average = grades_sum / len(students[student_name])
+        print(f"Moyenne de {student_name} : {round(average, 2)}")
+    else:
+        print(f"L'étudiant {student_name} n'existe pas dans la liste.")
+
+
+if __name__ == '__main__':
+    main()
