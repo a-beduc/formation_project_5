e6b1802a97c792e48421760c2c24530d7774fba7
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-10-04 09:53:41 +0200

    Exercice 9

--- Exercice09/main.py
+++ Exercice09/main.py
@@ -1,6 +1,23 @@
-# Test de la classe Rectangle
-rectangle = Rectangle(5, 3) # 5:width & 3:length
-print("Largeur:", rectangle.width)
-print("Longueur:", rectangle.length)
-print("Aire:", rectangle.calculate_area())
-print("Périmètre:", rectangle.calculate_perimeter())
+class Rectangle:
+    def __init__(self, width, length):
+        self.width = width
+        self.length = length
+
+    def calculate_area(self):
+        return self.width * self.length
+
+    def calculate_perimeter(self):
+        return 2 * (self.width + self.length)
+
+
+def main():
+    # Test de la classe Rectangle
+    rectangle = Rectangle(5, 3)  # 5:width & 3:length
+    print("Largeur:", rectangle.width)
+    print("Longueur:", rectangle.length)
+    print("Aire:", rectangle.calculate_area())
+    print("Périmètre:", rectangle.calculate_perimeter())
+
+
+if __name__ == '__main__':
+    main()
